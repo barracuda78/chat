237c46df12778c27e5d44c0988fce97d8ed19abc
Andrey Ruzaev <barracuda78@yandex.ru>
2021-02-28 23:19:30 +0300

    test sql connection and Statement execution

--- src/com/barracuda/database/Connector.java
+++ src/com/barracuda/database/Connector.java
@@ -1,13 +1,15 @@
 package com.barracuda.database;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Enumeration;
 
 public class Connector {
     private static Enumeration<Driver> drivers = DriverManager.getDrivers();
+    private static Statement statement;
+    private final static String query = "CREATE TABLE test (" +
+            "id INT," +
+            "name VARCHAR(30)" +
+            "info VARCHAR(100))";
 
     //testing
 //    public static void main(String[] args) {
@@ -27,4 +29,26 @@ public class Connector {
         }
         return null;
     }
+
+    public static void executeQuery(String query){
+        Connection connection = getConnection("", "user", "12345");
+        try {
+            statement = connection.createStatement();
+            boolean b =statement.execute(query);
+
+            if(b){
+                ResultSet rs = statement.getResultSet();
+            }
+            else{
+                int count = statement.getUpdateCount();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    //step 3: statement
+
 }
